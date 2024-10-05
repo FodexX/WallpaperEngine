32a6db0e1ce3fb9f7b001b3dc43fbad9aab66ab0
a.boikov <alex8oikov@yandex.ru>
2024-10-05 11:15:57 +0300

    добавил комментарии по REVIEW 1

--- TestWallpaper/TestWallpaperTag.cs
+++ TestWallpaper/TestWallpaperTag.cs
@@ -6,10 +6,14 @@ namespace TestWallpaper
     public class TestWallpaperTag
     {
         [TestMethod]
+        /// REVIEW. a.boikov. 05/10/2024. Объединить два тест-кейса Test_GetWallpapersByTag_WithImagePaths и Test_GetWallpapersByTag_NoResults_WithImagePaths
+        /// в один, управляемый данными.
+        /// В тест добавить проверку существующих - несуществующих файлов. (лучше отдельным тестом)
         public void Test_GetWallpapersByTag_WithImagePaths()
         {
             var selector = new WallpaperSelector();
 
+            /// REVIEW. a.boikov. 05/10/2024. Пути нужно указывать относительные (от собранного exe с тестами)
             var natureWallpaper = new Wallpaper("Лес", "\"C:\\Users\\fodex\\source\\repos\\WallpaperEngine\\Picture\\Nature\\1.jpg\"", new List<string> { "Природа", "Зелень" });
             var sportsWallpaper = new Wallpaper("Футбол", "\"C:\\Users\\fodex\\source\\repos\\WallpaperEngine\\Picture\\Footbool\\1.jpg\"", new List<string> { "Спорт", "Действие" });
 
