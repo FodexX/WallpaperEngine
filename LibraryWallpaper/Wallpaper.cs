04528b3f9e0dca71860cd388474f2fa856b9be42
fodexx <mrdeaf123@nail.ru>
2024-10-05 10:58:26 +0300

    add picture

--- LibraryWallpaper/Wallpaper.cs
+++ LibraryWallpaper/Wallpaper.cs
@@ -3,11 +3,13 @@
     public class Wallpaper
     {
         public string Name { get; set; }
+        public string ImagePath { get; set; }
         public List<string> Tags { get; set; }
 
-        public Wallpaper(string name, List<string> tags)
+        public Wallpaper(string name, string imagePath, List<string> tags)
         {
             Name = name;
+            ImagePath = imagePath;
             Tags = tags;
         }
     }
--- TestWallpaper/TestWallpaperTag.cs
+++ TestWallpaper/TestWallpaperTag.cs
@@ -1,36 +1,39 @@
 ﻿using LibraryWallpaper;
 
 namespace TestWallpaper
-{ggf
+{
     [TestClass]
     public class TestWallpaperTag
     {
         [TestMethod]
-        public void Test_GetWallpapersByTag()
+        public void Test_GetWallpapersByTag_WithImagePaths()
         {
             var selector = new WallpaperSelector();
 
-            var natureWallpaper = new Wallpaper("Ëåñ", new List<string> { "Ïðèðîäà", "Çåëåíü" });
-            var sportsWallpaper = new Wallpaper("Ôóòáîë", new List<string> { "Ñïîðò", "Äåéñòâèå" });
+            var natureWallpaper = new Wallpaper("Лес", "\"D:\\Picture\\Nature\\1.jpg\"", new List<string> { "Природа", "Зелень" });
+            var sportsWallpaper = new Wallpaper("Футбол", "\"D:\\Picture\\Footbool\\1.jpg\"", new List<string> { "Спорт", "Действие" });
+
             selector.AddWallpaper(natureWallpaper);
             selector.AddWallpaper(sportsWallpaper);
 
-            var result = selector.GetWallpapersByTag("Ïðèðîäà");
+            var result = selector.GetWallpapersByTag("Природа");
 
             Assert.AreEqual(1, result.Count);
-            Assert.AreEqual("Ëåñ", result[0].Name);
+            Assert.AreEqual("Лес", result[0].Name);
+            Assert.AreEqual("\"D:\\Picture\\Nature\\1.jpg\"", result[0].ImagePath);
         }
 
         [TestMethod]
-        public void Test_GetWallpapersByTag_NoResults()
+        public void Test_GetWallpapersByTag_NoResults_WithImagePaths()
         {
             var selector = new WallpaperSelector();
-            var natureWallpaper = new Wallpaper("Ãîðû", new List<string> { "Ïðèðîäà" });
+            var natureWallpaper = new Wallpaper("Горы", "\"D:\\Picture\\Nature\\1.jpg\"", new List<string> { "Природа" });
             selector.AddWallpaper(natureWallpaper);
 
-            var result = selector.GetWallpapersByTag("Ñïîðò");
+            var result = selector.GetWallpapersByTag("Лес");
 
             Assert.AreEqual(0, result.Count);
         }
+
     }
 }
