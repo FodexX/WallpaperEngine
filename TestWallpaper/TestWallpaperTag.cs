49ec4b1b36b8cb434302cfc9e83aad1e7aede46d
fodexx <mrdeaf123@nail.ru>
2024-10-05 11:57:49 +0300

    combining test cases

--- TestWallpaper/TestWallpaperTag.cs
+++ TestWallpaper/TestWallpaperTag.cs
@@ -11,32 +11,27 @@ namespace TestWallpaper
         /// В тест добавить проверку существующих - несуществующих файлов. (лучше отдельным тестом)
         public void Test_GetWallpapersByTag_WithImagePaths()
         {
-            var selector = new WallpaperSelector();
+            var selector1 = new WallpaperSelector();
+            var selector2 = new WallpaperSelector();
 
             /// REVIEW. a.boikov. 05/10/2024. Пути нужно указывать относительные (от собранного exe с тестами)
-            var natureWallpaper = new Wallpaper("Лес", "\"C:\\Users\\fodex\\source\\repos\\WallpaperEngine\\Picture\\Nature\\1.jpg\"", new List<string> { "Природа", "Зелень" });
-            var sportsWallpaper = new Wallpaper("Футбол", "\"C:\\Users\\fodex\\source\\repos\\WallpaperEngine\\Picture\\Footbool\\1.jpg\"", new List<string> { "Спорт", "Действие" });
+            var natureWallpaper = new Wallpaper("Лес", "Picture/Nature/1.jpg", new List<string> { "Природа", "Зелень" });
+            var sportsWallpaper = new Wallpaper("Футбол", "Picture/Football/1.jpg", new List<string> { "Спорт", "Действие" });
+            var mountWallpaper = new Wallpaper("Горы", "Picture/Nature/1.jpg", new List<string> { "Природа" });
 
-            selector.AddWallpaper(natureWallpaper);
-            selector.AddWallpaper(sportsWallpaper);
+            selector1.AddWallpaper(natureWallpaper);
+            selector1.AddWallpaper(sportsWallpaper);
+            selector2.AddWallpaper(mountWallpaper);
 
-            var result = selector.GetWallpapersByTag("Природа");
+            var result1 = selector1.GetWallpapersByTag("Природа");
 
-            Assert.AreEqual(1, result.Count);
-            Assert.AreEqual("Лес", result[0].Name);
-            Assert.AreEqual("\"C:\\Users\\fodex\\source\\repos\\WallpaperEngine\\Picture\\Nature\\1.jpg\"", result[0].ImagePath);
-        }
+            Assert.AreEqual(1, result1.Count);
+            Assert.AreEqual("Лес", result1[0].Name);
+            Assert.AreEqual("Picture/Nature/1.jpg", result1[0].ImagePath);
 
-        [TestMethod]
-        public void Test_GetWallpapersByTag_NoResults_WithImagePaths()
-        {
-            var selector = new WallpaperSelector();
-            var natureWallpaper = new Wallpaper("Горы", "\"C:\\Users\\fodex\\source\\repos\\WallpaperEngine\\Picture\\Nature\\1.jpg\"", new List<string> { "Природа" });
-            selector.AddWallpaper(natureWallpaper);
+            var result2 = selector2.GetWallpapersByTag("Спорт");
 
-            var result = selector.GetWallpapersByTag("Спорт");
-
-            Assert.AreEqual(0, result.Count);
+            Assert.AreEqual(0, result2.Count);
         }
 
         [TestMethod]
