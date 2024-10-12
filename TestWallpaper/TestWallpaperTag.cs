cbb0ff6abf2332ef4234888536fbf144aed4ec7b
fodexx <mrdeaf123@nail.ru>
2024-10-12 08:11:11 +0300

    test datarow

--- TestWallpaper/TestWallpaperTag.cs
+++ TestWallpaper/TestWallpaperTag.cs
@@ -1,65 +1,65 @@
 ﻿using LibraryWallpaper;
+using System.IO;
 
 namespace TestWallpaper
 {
     [TestClass]
     public class TestWallpaperTag
     {
-        [TestMethod]
-        /// REVIEW. a.boikov. 05/10/2024. Объединить два тест-кейса Test_GetWallpapersByTag_WithImagePaths и Test_GetWallpapersByTag_NoResults_WithImagePaths
-        /// в один, управляемый данными.
-        /// В тест добавить проверку существующих - несуществующих файлов. (лучше отдельным тестом)
-        public void Test_GetWallpapersByTag_WithImagePaths()
+        [DataTestMethod]
+        [DataRow("Природа", 2, "Лес", "Picture/Nature/1.jpg")]
+        [DataRow("Спорт", 1, "Футбол", "Picture/Football/1.jpg")]
+        [DataRow("Действие", 1, "Футбол", "Picture/Football/1.jpg")]
+        [DataRow("Горы", 0, null, null)] // No matching wallpapers
+        public void Test_GetWallpapersByTag_WithImagePaths(string tag, int expectedCount, string expectedName, string expectedImagePath)
         {
-            var selector1 = new WallpaperSelector();
-            var selector2 = new WallpaperSelector();
+            var selector = new WallpaperSelector();
 
-            /// REVIEW. a.boikov. 05/10/2024. Пути нужно указывать относительные (от собранного exe с тестами)
             var natureWallpaper = new Wallpaper("Лес", "Picture/Nature/1.jpg", new List<string> { "Природа", "Зелень" });
             var sportsWallpaper = new Wallpaper("Футбол", "Picture/Football/1.jpg", new List<string> { "Спорт", "Действие" });
-            var mountWallpaper = new Wallpaper("Горы", "Picture/Nature/1.jpg", new List<string> { "Природа" });
+            var mountWallpaper = new Wallpaper("Горы", "Picture/Nature/2.jpg", new List<string> { "Природа" });
 
-            selector1.AddWallpaper(natureWallpaper);
-            selector1.AddWallpaper(sportsWallpaper);
-            selector2.AddWallpaper(mountWallpaper);
-
-            var result1 = selector1.GetWallpapersByTag("Природа");
-
-            Assert.AreEqual(1, result1.Count);
-            Assert.AreEqual("Лес", result1[0].Name);
-            Assert.AreEqual("Picture/Nature/1.jpg", result1[0].ImagePath);
-
-            var result2 = selector2.GetWallpapersByTag("Спорт");
-
-            Assert.AreEqual(0, result2.Count);
-        }
-
-        [TestMethod]
-        public void Test_GetWallpapersByTag()
-        {
-            var selector = new WallpaperSelector();
-
-            var natureWallpaper = new Wallpaper("Лес", " ", new List<string> { "Природа", "Зелень" });
-            var sportsWallpaper = new Wallpaper("Футбол"," " , new List<string> { "Спорт", "Действие" });
             selector.AddWallpaper(natureWallpaper);
             selector.AddWallpaper(sportsWallpaper);
+            selector.AddWallpaper(mountWallpaper);
 
-            var result = selector.GetWallpapersByTag("Природа");
+            var result = selector.GetWallpapersByTag(tag);
 
-            Assert.AreEqual(1, result.Count);
-            Assert.AreEqual("Лес", result[0].Name);
+            Assert.AreEqual(expectedCount, result.Count);
+
+            if (expectedCount > 0)
+            {
+                Assert.AreEqual(expectedName, result[0].Name);
+                Assert.AreEqual(expectedImagePath, result[0].ImagePath);
+            }
         }
 
         [TestMethod]
-        public void Test_GetWallpapersByTag_NoResults()
+        public void Test_CheckIfImagePathExists()
         {
             var selector = new WallpaperSelector();
-            var natureWallpaper = new Wallpaper("Горы", " ", new List<string> { "Природа" });
-            selector.AddWallpaper(natureWallpaper);
 
-            var result = selector.GetWallpapersByTag("Спорт");
+            var validWallpaper = new Wallpaper("Лес", "Picture/Nature/1.jpg", new List<string> { "Природа" });
+            var invalidWallpaper = new Wallpaper("Футбол", "Picture/InvalidPath/1.jpg", new List<string> { "Спорт" });
 
-            Assert.AreEqual(0, result.Count);
+            selector.AddWallpaper(validWallpaper);
+            selector.AddWallpaper(invalidWallpaper);
+
+            foreach (var wallpaper in selector.Wallpapers)
+            {
+                bool fileExists = File.Exists(wallpaper.ImagePath);
+                if (fileExists)
+                {
+                    Console.WriteLine($"Файл существует: {wallpaper.ImagePath}");
+                }
+                else
+                {
+                    Console.WriteLine($"Файл не найден: {wallpaper.ImagePath}");
+                }
+
+                // Для тестов можно сделать ассерт на существование файла:
+                // Assert.IsTrue(fileExists, $"Файл {wallpaper.ImagePath} не существует.");
+            }
         }
     }
 }
