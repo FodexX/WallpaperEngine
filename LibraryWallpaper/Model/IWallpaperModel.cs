0100922d7c10959094b000440bf6787fe0ff9cfd
fodexx <mrdeaf123@nail.ru>
2024-11-21 12:59:57 +0300

    mvp demo_version

---
+++ LibraryWallpaper/Model/IWallpaperModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryWallpaper.Model
+{
+    public interface IWallpaperModel
+    {
+        List<Wallpaper> GetWallpaper();
+        
+        int CountWallpaper();
+
+        event Action<Wallpaper> WallpaperLoaded;
+    }
+}
---
+++ LibraryWallpaper/Model/WallpapersModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryWallpaper.Model
+{
+    public class WallpapersModel : IWallpaperModel
+    {
+        private List<Wallpaper> wallpapers_ = new List<Wallpaper>();
+
+        public WallpapersModel() 
+        {
+            wallpapers_.Add(new Wallpaper
+            {
+                Name = "Cпортик",
+                ImagePath = " ",
+                Tags = new List<string> { "Футбол", "Мяч" },
+            });
+        }
+
+
+        public List<Wallpaper> GetWallpapers()
+        {
+            return wallpapers_;
+        }
+
+        public int CountWallpaper()
+        { 
+            return wallpapers_.Count; 
+        }
+
+        public void NotifyClientsUpdated()
+        {
+            WallpaperLoaded?.Invoke();
+        }
+
+        public event Action WallpaperLoaded;
+
+    }
+}
